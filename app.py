f08e43e70622c48fe9ebdcdefe5eef0377079420
Lisa Komidar <lkomidar@fusionstorm.com>
2020-03-03 10:49:13 -0500

    Update urls for files

--- app.py
+++ app.py
@@ -5,27 +5,10 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
 @app.route("/")
 def home():
     return render_template('index.html')
     
-    ##return "Hello World!"
-
-
-##class HelloWorld(Resource):
-##    def get(self):
-##        return {'hello': 'world'}
-
-##api.add_resource(HelloWorld, '/')
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
     
-    
-##@app.route('/')
-##def hello_world():
-    ##return render_template('index.html')
-
-##if __name__ == '__main__':
-##    app.run(debug=True)
